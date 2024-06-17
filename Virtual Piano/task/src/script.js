e6714aa11e41e8bbe644618cf1d20839176c619b
Brobo1 <armanahz95@gmail.com>
2024-06-17 15:53:32 +0200

    Update .gitignore, adjust script.js, and modify task-info.yaml
    The .gitignore file has been updated to include more ignored directories. The 'keydown' and 'keyup' event listeners in script.js have been refactored to simplify the interaction with keys on the page, and the task-info.yaml file time value has been updated.

--- Virtual Piano/task/src/script.js
+++ Virtual Piano/task/src/script.js
@@ -1,37 +1,29 @@
-let wKeys = document.querySelectorAll(".white-keys kbd");
-let bKeys = document.querySelectorAll(".black-keys kbd");
+let keys = document.querySelectorAll("kbd");
 
 document.addEventListener("keydown", (e) => {
   let isKey = false;
-  wKeys.forEach((key) => {
+  keys.forEach((key) => {
     if (key.innerText === e.key.toUpperCase()) {
       key.style.backgroundColor = "#6a6a6a";
       isKey = true;
-      const audio = new Audio(`white_keys/${e.key}.mp3`);
+      let keyColor = key.parentElement.className.split("-")[0];
+      const audio = new Audio(`${keyColor}_keys/${e.key}.mp3`);
       audio.play();
       audio.volume = 0.2;
     }
   });
-  bKeys.forEach((key) => {
-    if (key.innerText === e.key.toUpperCase()) {
-      key.style.backgroundColor = "#6a6a6a";
-      isKey = true;
-      const audio = new Audio(`black_keys/${e.key}.mp3`);
-      audio.play();
-      audio.volume = 0.2;
-    }
-  });
-
   isKey
     ? console.log(`The '${e.key.toUpperCase()}' key is pressed`)
     : console.warn("Unbound key");
 });
 
 document.addEventListener("keyup", () => {
-  wKeys.forEach((key) => {
-    key.style.backgroundColor = "#ffffff";
-  });
-  bKeys.forEach((key) => {
-    key.style.backgroundColor = "#000000";
+  keys.forEach((key) => {
+    let keyColor = key.parentElement.className.split("-")[0];
+    if (keyColor === "white") {
+      key.style.backgroundColor = "#ffffff";
+    } else {
+      key.style.backgroundColor = "#000000";
+    }
   });
 });
